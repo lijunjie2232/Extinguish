9330c30b20349b54ce2ba280ace4cf1b0d8fbfb0
lijunjie2232 <2533584225@qq.com>
2026-06-20 09:31:52 +0900

    chore: enable Kotlin serialization plugin and ignore third/
    - Register org.jetbrains.kotlin.plugin.serialization in root and app modules
    - Add serialization plugin alias to the version catalog
    - Ignore third/ directory in .gitignore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.serialization)
     id(libs.plugins.parcelize.get().pluginId)
 }
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ plugins {
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.serialization) apply false
     alias(libs.plugins.jetbrainsKotlinJvm) apply false
 }
 true
